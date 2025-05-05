d1c3d661f827c9feecc5facc4c7acb0262a6691d
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2025-05-05 07:50:19 +0530

    feat: add zod for schema validation and drizzle migration files
    Introduce zod for schema validation in sign-in and sign-up forms. Add drizzle migration files to manage database schema changes, including the creation of the `files` table. This ensures better data validation and structured database management.

---
+++ schemas/signInSchema.ts
@@ -0,0 +1,6 @@
+import * as z from "zod";
+
+export const signInSchema = z.object({
+    email : z.string().min(1, { message: "Email is required" }).email({ message: "Invalid email and please enter a valid email" }),
+    password : z.string().min(1, { message: "Password is required" }).min(8, { message: "Password must be at least 8 characters long" })
+})
---
+++ schemas/signUpSchema.ts
@@ -0,0 +1,16 @@
+import * as z from "zod";
+
+export const signUpSchema = z.object({
+  email: z
+    .string()
+    .min(1, { message: "Email is required" })
+    .email({ message: "Invalid email and please enter a valid email" }),
+  password: z
+    .string()
+    .min(1, { message: "Password is required" })
+    .min(8, { message: "Password must be at least 8 characters long" }),
+    passwordConfirmation: z.string().min(1, { message: "Confirm password is required" }).min(8, { message: "Confirm password must be at least 8 characters long" })
+}).refine((data) => data.password === data.passwordConfirmation, {
+  message: "Passwords do not match",
+  path: ["passwordConfirmation"],
+})
